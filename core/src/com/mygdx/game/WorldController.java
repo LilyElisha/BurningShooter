6f1df2b6086152430e9784ace6bc8df490089b2a
Lily Peterson <lilypeterson803@gmail.com>
2019-01-09 08:32:18 -0800

    world controller works hopefully

--- core/src/com/mygdx/game/WorldController.java
+++ core/src/com/mygdx/game/WorldController.java
@@ -3,7 +3,7 @@ package com.mygdx.game;
 public class WorldController { //can pe package private
 	public static void Scroll(){ //can be package private whatever the heck that is
 		
-		main.backgroundSpriteX = main.playerX;
+		/*main.backgroundSpriteX = main.playerX;
 
 		if(main.playerX == main.backgroundSpriteX) { //always true
 			main.backgroundSprite2X = main.backgroundSpriteX - main.canvisWidth;
@@ -15,7 +15,17 @@ public class WorldController { //can pe package private
 		
 		if(main.playerX == main.backgroundSprite2X-main.canvisWidth) {
 			main.backgroundSprite3X = main.backgroundSprite2X - main.canvisWidth;
+		}*/
+		
+		//
+		if(main.backgroundX =< main.canvisWidth) {
+		     main.background2X = main.backgroundX - main.canvisWidth;
+		} else if (main.backgroundX => main.canvisWidth) {
+		     main.backgroundX = 0;
+		     main.background2X = main.backgroundX - main.canvisWidth;
 		}
+		
+		//
 		
 		// if the backround x is at the start of the canvis then we want to put the other canvis behind it and doing it by taking the canvis x and dividing is by the canvis width and if we get a whole number then we will put the one down accordingly weather its even or odd like if the x is 8000 then 8000/800(canviswidth) will br 10 and sence its even it will move backround 2 behind backround one
 		
