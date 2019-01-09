404835ad6ede3194c4076b7d3973b75c3366991d
Lilyp803 <lilypeterson803@gmail.com>
2019-01-08 16:57:39 -0800

    before i left

--- core/src/com/mygdx/game/WorldController.java
+++ core/src/com/mygdx/game/WorldController.java
@@ -17,7 +17,7 @@ public class WorldController { //can pe package private
 			main.backgroundSprite3X = main.backgroundSprite2X - main.canvisWidth;
 		}
 		
-		//
+		// if the backround x is at the start of the canvis then we want to put the other canvis behind it and doing it by taking the canvis x and dividing is by the canvis width and if we get a whole number then we will put the one down accordingly weather its even or odd like if the x is 8000 then 8000/800(canviswidth) will br 10 and sence its even it will move backround 2 behind backround one
 		
 		//if(main.playerX/main.canvisWidth )
 		
