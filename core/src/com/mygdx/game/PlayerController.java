31a36c40da7f3cc65a9cb2db62a7735843cf8744
Meelock <elisha.pommert@gmail.com>
2019-01-09 23:19:25 -0800

    Fixed broken merge, sorry lily.

--- core/src/com/mygdx/game/PlayerController.java
+++ core/src/com/mygdx/game/PlayerController.java
@@ -6,15 +6,29 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 
 
 public class PlayerController {
+    public static int speed = 1;
+    public static int jump = 1;
+    private static boolean jumps = true;
 
     public static void keyInput() {
         if (Gdx.input.isKeyPressed(Keys.A)) {
-            main.playerX++;
-            main.backgroundSpriteX++;
+            main.playerX += speed;
+            main.backgroundSpriteX += speed;
         }
         if (Gdx.input.isKeyPressed(Keys.D)) {
-            main.playerX--;
-            main.backgroundSpriteX--;
+            main.playerX -= speed;
+            main.backgroundSpriteX -= speed;
+        }
+        if (Gdx.input.isKeyPressed(Keys.W) && jumps == true) {
+            jumps = false;
+            for (int i = 5; i > 0; i--) {
+                main.playerY += i;
+            }
+            System.out.print("l");
+            for (int l = 5; l > 0; l--) {
+                main.playerY -= l;
+            }
+            jumps = true;
         }
     }
 
@@ -22,6 +36,6 @@ public class PlayerController {
 
         main.player = new Sprite(main.texture);
         main.playerX = (main.canvisWidth * 0);
-        main.playerY = main.canvisHeight; //can be 0
+        main.playerY = (main.canvisHeight * 0); //can be 0
     }
 }
