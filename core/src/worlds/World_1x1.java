9486314b66ca91c5b4f8f49f73d3300aa97a1909
Meelock <elisha.pommert@gmail.com>
2019-01-09 22:30:25 -0800

    I have no idea how this works, so this is a stop gap till I can figure out how to clean stuff up.

--- core/src/worlds/World_1x1.java
+++ core/src/worlds/World_1x1.java
@@ -1,12 +1,24 @@
 package worlds;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.files.FileHandle;
 import com.mygdx.game.WorldObjects;
+import com.mygdx.game.WorldSetup;
 
 
 public class World_1x1 {
     public void go() {
 
-//        WorldSetup.WorldTexture = "sunny ground.png";
+        FileHandle file = Gdx.files.internal("World_1x1.world");
+        String st = "";
+        try {
+            st = file.readString();
+            System.out.println(st);
+        } catch (Exception e) {
+            System.out.println("No world found");
+        }
+
+        WorldSetup.WorldTexture = st;
     }
 
 
